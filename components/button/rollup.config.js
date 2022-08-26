f3c2d560750f6b460c25fd2883e4b8f4bae9499c
Jian Liao <jianliao@adobe.com>
2022-08-26 09:06:39 -0700

    Hand written button component

--- components/button/rollup.config.js
+++ components/button/rollup.config.js
@@ -1,20 +1,26 @@
-import typescript from '@rollup/plugin-typescript';
+import del from "rollup-plugin-delete";
 import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
   {
-     input: './index.ts',
-     output: {
-       dir: 'dist',
-       format: 'esm',
-       sourcemap: true
-     },
-     plugins: [typescript()]
+    external: ['react', 'next/dynamic', '@lit-labs/react', /^@spectrum-web-components.*/],
+    input: './index.ts',
+    output: {
+      dir: 'dist',
+      format: 'esm',
+      sourcemap: 'inline'
+    },
+    plugins: [
+      typescript({ inlineSources: true, tsconfig: './tsconfig.json', declarationDir: `${__dirname}/dist/types` }),
+    ]
   },
   {
-     input: 'dist/index.d.ts',
-     output: [{ file: 'dist/index.d.ts', format: "esm" }],
-     external: [/\.css$/],
-     plugins: [dts()],
+    input: `${__dirname}/dist/types/index.d.ts`,
+    output: [{ file: `${__dirname}/dist/index.d.ts`, format: "esm" }],
+    plugins: [
+      dts(),
+      del({ targets: "dist/types", hook: "buildEnd" })
+    ]
   }
 ];
