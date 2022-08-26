328e4e0f76e275added71ea207d0e439dae85684
Jian Liao <jianliao@adobe.com>
2022-08-26 11:38:38 -0700

    Hand written button-group component

---
+++ components/button-group/ButtonGroup.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { createComponent } from '@lit-labs/react';
+import { ButtonGroup } from '@spectrum-web-components/button-group';
+import '@spectrum-web-components/button-group/sp-button-group.js';
+
+export const SpButtonGroup = createComponent(React, 'sp-button-group', ButtonGroup, { }, 'SpButtonGroup');
---
+++ components/button-group/rollup.config.js
@@ -0,0 +1,24 @@
+import del from 'rollup-plugin-delete';
+import dts from 'rollup-plugin-dts';
+import typescript from '@rollup/plugin-typescript';
+
+export default [
+  {
+    external: ['react', 'next/dynamic', '@lit-labs/react', /^@spectrum-web-components.*/],
+    input: './index.ts',
+    output: [
+      { dir: 'dist', format: 'es', sourcemap: 'inline' }
+    ],
+    plugins: [
+      typescript({ inlineSources: true, tsconfig: './tsconfig.json', declarationDir: `${__dirname}/dist/types` }),
+    ]
+  },
+  {
+    input: `${__dirname}/dist/types/index.d.ts`,
+    output: [{ file: `${__dirname}/dist/index.d.ts`, format: 'es' }],
+    plugins: [
+      dts(),
+      del({ targets: 'dist/types', hook: 'buildEnd' })
+    ]
+  }
+];
